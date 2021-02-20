c40d6db7931e3f52b2822161d3e3e03280c926af
sadamdamz <sadamdamzzz@gmail.com>
2021-02-21 03:27:25 +0530

    host and join meeting api

--- server/controllers/firebase.js
+++ server/controllers/firebase.js
@@ -7,27 +7,60 @@ const getUser = async(req, res) => {
 
 const meetingID = async(req, res) => {
   const {uid} = req.body;
+  const meetingId = getMeetingId();
   let postData = {
     uid,
     host:false,
-    meetingId:getMeetingId(),
+    meetingId:meetingId,
   }
   try {
     await db.ref('users/'+uid).once('value').then((snapshot)=>{
-      let value = snapshot.val();
-      if(value==null){
+      if(snapshot.exists()){
+        res.send(snapshot.val());
+      }else{
         db.ref('users/'+uid).set(postData);
         res.send(postData);
-      }else{
-        res.send(value)
       }
     });
+    await db.ref('meetings/'+meetingId).set({host:false});
   } catch (error) {
-    res.send(error);
+    res.send({status:501,error:error});
+  }
+}
+
+const hostMeetingController = async(req, res) => {
+  const {meetingId,host} = req.body;
+  try {
+    let data = await db.ref('meetings/'+meetingId).set({host:host});
+    console.log(data);
+    res.send({status:200,message:'Meeting Started'});
+  } catch (error) {
+    res.send({status:501,error:error})
+  }
+}
+
+
+const joinRoom = async(req, res) =>{
+  const {meetingId} = req.body;
+  try {
+    let snapshot = await db.ref('meetings/'+meetingId).once('value');
+    if(snapshot.exists() && snapshot.val().host===false){
+      res.send({status:204,message:"Host Not Started the meeting"})
+    }
+    else if(snapshot.exists() && snapshot.val().host===true){
+      res.send({status:200, message:"You can Join Now"})
+    }
+    else{
+      res.send({status:205, message:"Meeting ID you Entered is Wrong"})
+    }
+  } catch (error) {
+    res.send({status:501,error:error});
   }
 }
 
 module.exports = {
   getUser,
-  meetingID
+  meetingID,
+  hostMeetingController,
+  joinRoom
 }
--- server/routes/firebase.js
+++ server/routes/firebase.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const firebase = require('../controllers/firebase');
 
-router.post('/meetingroom', firebase.meetingID);
 router.get('/', firebase.getUser);
+router.post('/meetingroom', firebase.meetingID);
+router.post('/joinroom', firebase.joinRoom);
+router.post('/hostMeetingController', firebase.hostMeetingController);
 
 module.exports = router;
