673b81d1f5bf39072389f65bce6e2cb519e34e5a
sadamdamz <sadamdamzzz@gmail.com>
2021-03-22 23:19:51 +0530

    https

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -27,7 +27,7 @@ function getIcalObjectInstance(
 
 const sendInvitation = async (postData) => {
   const {meetingId, mailTo, userName, date, topic, time, momentDate} = postData;
-  let url = `http://wedgrab.com/meetingroom?${meetingId}`;
+  let url = `https://wedgrab.com/meetingroom?${meetingId}`;
   let email = 'videochatapp.test@gmail.com'
   let calendarObj = getIcalObjectInstance(momentDate, topic, userName, url, email);
   console.log(calendarObj);
@@ -43,7 +43,7 @@ const sendInvitation = async (postData) => {
     from: 'videochatapp.test@gmail.com',
     to: mailTo,
     subject: 'Meeting Invitation from Video App',
-    html: `<h2>${userName} has sent you a invitation to join Meeting</h2></br><h5>Meeting Details</h5></br><p>Topic : ${topic}</p></br><ul><li>Meeting Link: <a href=http://wedgrab.com/meetingroom?${meetingId} >http://wedgrab.com/meetingroom?${meetingId}</a></li><li>Meeting ID: ${meetingId}</li><li>Scheduled at: ${date}</li><li>Time: ${time}</li></ul>`,
+    html: `<h2>${userName} has sent you a invitation to join Meeting</h2></br><h5>Meeting Details</h5></br><p>Topic : ${topic}</p></br><ul><li>Meeting Link: <a href=https://wedgrab.com/meetingroom?${meetingId} >https://wedgrab.com/meetingroom?${meetingId}</a></li><li>Meeting ID: ${meetingId}</li><li>Scheduled at: ${date}</li><li>Time: ${time}</li></ul>`,
   };
   body['alternatives'] = alternatives;
   body['alternatives']['contentType'] = 'text/calendar';
